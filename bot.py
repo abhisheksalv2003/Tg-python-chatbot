4da3d9a2268229e4ded5a23afd5b1e0f10da221c
abhisheksalv2003 <96788924+abhisheksalv2003@users.noreply.github.com>
2024-04-10 11:20:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 def main():
-    updater = Updater(token='6952359179:AAFhuz_Ry06UNoVn4VmYn8xiMrtscEYrZ6g', use_context=True)
+    updater = Updater(token='6952359179:AAFTB0lk6NLlweWLsx6stij5-IC7wehMzvM', use_context=True)
     dispatcher = updater.dispatcher
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
